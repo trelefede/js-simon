5e3d9aa181816f6be4da1568d4a968ab2641698a
TreleaniFederico <treleani.federico@gmail.com>
2022-09-05 16:43:21 +0200

    add

--- js/script.js
+++ js/script.js
@@ -6,3 +6,48 @@ console.log('JS OK!');
 // Dopo che sono stati inseriti i 5 numeri,
 // il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+const arrayNum = [];
+let userArrayNum = [];
+
+for (let i = 0; i < 5; i++) {
+    let number = Math.floor(Math.random() * 100);
+    arrayNum.push(number);
+}
+
+console.log(arrayNum);
+
+const numbers = document.querySelector(".numbers");
+numbers.innerHTML = arrayNum;
+
+let timerNone = setTimeout(function () {
+    numbers.classList.add("none");
+}, 5 * 1000);
+
+let timerPrompt = setTimeout(function () {
+    alert('Inserisci uno alla volta i numeri precedentemente mostrati');
+    for (let i = 0; i < arrayNum.length; i++) {
+        let userNum = parseInt(prompt('Inserisci il numero'));
+        userArrayNum.push(userNum);
+    }
+
+    console.log(userArrayNum);
+
+    for (i = 0; i < userArrayNum; i++) {
+        if (arrayNum.includes(userArrayNum[i])) {
+            index++;
+            console.log(i);
+            correctNumber += " " + i
+        }
+    }
+}, 6 * 1000);
+
+let index = 0;
+let correctNumber;
+
+
+
+
+
+
+
+
